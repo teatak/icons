225d6deef3c148d5611e58c3d50dcae6a27cfb41
Yang <yangglive@icloud.com>
2024-03-23 01:13:02 +0800

    change

---
+++ src/svgs/index.ts
@@ -0,0 +1,5 @@
+export { default as AdUnitsOutlined24Px } from "./AdUnitsOutlined24Px";
+export { default as AdUnitsRounded24Px } from "./AdUnitsRounded24Px";
+export { default as AdUnitsSharp24Px } from "./AdUnitsSharp24Px";
+export { default as AdbSharp24Px } from "./AdbSharp24Px";
+export { default as AdbTwoTone24Px } from "./AdbTwoTone24Px";
