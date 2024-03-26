7e193bdf1a521ac7f703ffacf88870b9dc665475
Yang <yangglive@icloud.com>
2024-03-26 20:24:16 +0800

    change

--- src/Svg.tsx
+++ src/Svg.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import classnames from "classnames"
 
 export const createSvg = (...path: any) => {
     return React.memo(React.forwardRef((props: any, ref: any) => {
         const { className, viewBox, ...rest } = props;
-        const classNames = 'tui-icon ' + className
-        return <svg className={classNames} viewBox={viewBox ? viewBox : "0 0 24 24"} {...rest} >
+        const classNames = classnames(
+            'tui-icon',
+            className,
+        ); return <svg className={classNames} viewBox={viewBox ? viewBox : "0 0 24 24"} {...rest} >
             {path}
         </svg>
     }))
